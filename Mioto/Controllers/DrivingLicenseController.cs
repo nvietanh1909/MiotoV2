a4e028cf13f1a5627270b96b9b40bb60a82d5713
nvietanh1909 <nvietanh.190904@example.com>
2024-09-14 19:21:30 +0700

    Test API

--- Mioto/Controllers/DrivingLicenseController.cs
+++ Mioto/Controllers/DrivingLicenseController.cs
@@ -1,93 +1,57 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
 public class DrivingLicenseController : Controller
 {
-    private readonly ApiService _apiService;
+    private readonly HttpClient _apiService;
 
     public DrivingLicenseController()
     {
-        _apiService = new ApiService();
+        _apiService = new HttpClient();
     }
 
-    public async Task<ActionResult> GetLicenseDetails(string username, string password, string maHoSo)
+    [HttpPost]
+    public async Task<ActionResult> CheckLicense(string session, string donViXuLy, string ngayNhan1, string ngayNhan2)
     {
-        // Lấy session
-        var sessionResponse = await _apiService.GetSessionAsync(username, password);
-        var session = ExtractSessionFromResponse(sessionResponse); 
+        string apiUrl = "ADAPTER_URL/mapi/g";
 
-        if (string.IsNullOrEmpty(session))
+        var requestBody = new
         {
-            return View("Error");
+            session = session,
+            service = "DanhSachHoSo",
+            donViXuLy = donViXuLy,
+            ngayNhan1 = ngayNhan1,
+            ngayNhan2 = ngayNhan2
+        };
+
+        var jsonBody = Newtonsoft.Json.JsonConvert.SerializeObject(requestBody);
+
+        var httpContent = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+        _apiService.DefaultRequestHeaders.Clear();
+        _apiService.DefaultRequestHeaders.Add("Charset", "utf-8");
+
+        try
+        {
+            var response = await _apiService.PostAsync(apiUrl, httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                return Content(responseData); 
+            }
+            else
+            {
+                return Content($"Error: {response.StatusCode}");
+            }
         }
-
-        // Tra cứu hồ sơ
-        var recordResponse = await _apiService.LookupRecordAsync(session, "TraCuuHoSo", "Mã Đơn Vị", maHoSo);
-        var recordDetails = ExtractRecordDetailsFromResponse(recordResponse); 
-
-        // Trả về kết quả cho view
-        return View(recordDetails);
-    }
-
-    private string ExtractSessionFromResponse(string responseBody)
-    {
-        // Xử lý JSON và trả về session
-        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
-        return jsonResponse.session;
-    }
-
-    private object ExtractRecordDetailsFromResponse(string responseBody)
-    {
-        // Xử lý JSON và trả về thông tin hồ sơ
-        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
-        return jsonResponse.data;
-    }
-}
-
-public class ApiService
-{
-    private readonly HttpClient _client;
-    private readonly string _baseAddress = "https://gplx.gov.vn/";
-
-    public ApiService()
-    {
-        _client = new HttpClient
+        catch (Exception ex)
         {
-            BaseAddress = new System.Uri(_baseAddress)
-        };
-    }
-
-    public async Task<string> GetSessionAsync(string username, string password)
-    {
-        var request = new HttpRequestMessage(HttpMethod.Post, "/mapi/g")
-        {
-            Content = new StringContent(
-                $"{{ \"username\": \"{username}\", \"password\": \"{password}\" }}",
-                Encoding.UTF8,
-                "application/json")
-        };
-
-        var response = await _client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
-        return responseBody; 
-    }
-
-    public async Task<string> LookupRecordAsync(string session, string service, string donViXuLy, string maHoSo)
-    {
-        var request = new HttpRequestMessage(HttpMethod.Post, "/mapi/g")
-        {
-            Content = new StringContent(
-                $"{{ \"session\": \"{session}\", \"service\": \"{service}\", \"donViXuLy\": \"{donViXuLy}\", \"mahoso\": \"{maHoSo}\" }}",
-                Encoding.UTF8,
-                "application/json")
-        };
-
-        var response = await _client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
-        return responseBody; 
+            return Content($"Exception: {ex.Message}");
+        }
     }
 }
